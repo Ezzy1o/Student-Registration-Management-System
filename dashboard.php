8426cc1cdae0f9b1375cd2427e16b233ecc66138
Ezzy1o <puteraaeman5105@gmail.com>
2025-11-09 16:39:14 +0800

    Add files via upload

--- dashboard.php
+++ dashboard.php
@@ -1,10 +1,6 @@
 <?php
 session_start();
 
-header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
-header("Pragma: no-cache");
-header("Expires: 0");
-
 if (!isset($_SESSION['username'])) {
     header("Location: login.php");
     exit();
